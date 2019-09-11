7dc4fc9e41bf726152d057c046cf3c716957fe96
Bertik23 <39457484+Bertik23@users.noreply.github.com>
2019-09-11 20:56:19 +0200

    Update uno.py

--- uno.py
+++ uno.py
@@ -54,8 +54,8 @@ if (input("Login? ") == "a"):
     username = input('Username: ')
     password = getpass.getpass('Password: ')
 else:
-    username = "0306252184"
-    password = "3rthmywp"
+    username = ""
+    password = ""
 
 client = pybakalib.client.BakaClient(url)
 client.login(username, password)
